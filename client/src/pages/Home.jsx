2d09883c26e7d37c7c901c081672424f005a3651
chaudhuree <67362262+chaudhuree@users.noreply.github.com>
2023-05-01 20:56:41 +0600

    fontend filtering
    document.querySelectorAll('tbody tr') is usd for fontend filtering
    while search text match with item innerText then display otherwise display none

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -55,7 +55,14 @@ export const Home = () => {
   const handlePageClick = (e) => {
     setpageNo(e.selected + 1)
   }
+  
+  // fontend filter
+  const handleFiter = (e) => {
+    document.querySelectorAll('tbody tr').forEach((item) => {
+      item.innerText.toLowerCase().includes(e.target.value.toLowerCase()) ? item.style.display = "" : item.style.display = "none"
 
+    })
+  }
   return (
 
     <div className="grid justify-center items-center h-full my-16 px-20 ">
@@ -75,6 +82,11 @@ export const Home = () => {
         <div>
           <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Search Query:</label>
           <input onChange={handleSearch} type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search.."></input>
+        </div>
+        
+        <div>
+          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Filter Item:</label>
+          <input onChange={handleFiter} type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search.."></input>
         </div>
 
       </div>
